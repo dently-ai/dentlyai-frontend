30c610b54b601774c36de7c9aa06a9375363ca45
Nikola Miljkovic <nimiljko@microsoft.com>
2023-05-06 20:00:03 +0200

    a

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '@blueprintjs/core/lib/css/blueprint.css';
-import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import * as styles from '@blueprintjs/core/lib/css/blueprint.css';
+import * as styles2 from '@blueprintjs/icons/lib/css/blueprint-icons.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
